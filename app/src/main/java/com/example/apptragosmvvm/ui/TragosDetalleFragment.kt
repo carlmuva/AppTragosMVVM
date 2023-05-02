72887598e5f788f45fc5e856a28a9c0fc10ead69
CarlosMurillo <carlmuva@hotmail.com>
2023-05-02 16:11:49 -0600

    pantalla de detalles

--- app/src/main/java/com/example/apptragosmvvm/ui/TragosDetalleFragment.kt
+++ app/src/main/java/com/example/apptragosmvvm/ui/TragosDetalleFragment.kt
@@ -6,19 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.apptragosmvvm.R
 import com.example.apptragosmvvm.data.model.Drink
+import com.example.apptragosmvvm.databinding.FragmentMainBinding
+import com.example.apptragosmvvm.databinding.FragmentTragosDetalleBinding
 
 
 class TragosDetalleFragment : Fragment() {
 
     private lateinit var drink: Drink
+    private lateinit var mBinding: FragmentTragosDetalleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requireArguments().let {
             drink = it.getParcelable<Drink>("drink")!!
-            Log.d("DETALLES_FRAG", "${drink.toString()}")
+            //Log.d("DETALLES_FRAG", "${drink.toString()}")
         }
     }
 
@@ -27,7 +31,22 @@ class TragosDetalleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tragos_detalle, container, false)
+        mBinding = FragmentTragosDetalleBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(requireContext()).load(drink.imagen).centerCrop().into(mBinding.imgTrago)
+        mBinding.tragoTitle.text = drink.nombre
+        mBinding.tragoDesc.text = drink.descripcion
+        if(drink.hasAlcohol == "Non_Alcoholic"){
+            mBinding.txtHasAlcohol.text = "Bebida sin alcohol"
+        }else{
+            mBinding.txtHasAlcohol.text = "Bebida con alcohol"
+        }
+
+
     }
 
 
